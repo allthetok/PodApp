58cf7af97a5d11600b339164b925fa12f3a45344
allthetok <al.tokjuman@gmail.com>
2023-06-30 11:11:39 -0400

    build podcast detail component

--- src/components/PodcastDetail.js
+++ src/components/PodcastDetail.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './PodcastDetail.css'
 import filter from '../mockdata/searchpod'
-import {MoreVert, PlaylistAdd} from '@mui/icons-material'
+import {MoreVert, Padding, PlaylistAdd} from '@mui/icons-material'
 
 const PodcastDetail = () => {
 
     const formattedTime = (seconds) => {
         const hours = Math.floor(seconds/3600)
         const mins = Math.floor(seconds % 3600 / 60)
-        //const sec = Math.floor(seconds % 3600 % 60)
         const hoursDisplay = hours > 0 ? `${hours}h` : ''
         const minsDisplay = mins > 0 ? `${mins}m` : ''
-        //const secDisplay = sec > 0 ? `${sec}s` : ''
-
         return `${hoursDisplay}:${minsDisplay}`
     }
+
+    const formattedDateShort = inpDate => new Date(inpDate).toLocaleDateString('en-us', { year: 'numeric', month: 'long'})
+    const formattedDateLong = inpDate => new Date(inpDate).toLocaleDateString('en-us', { year: 'numeric', 'month': 'long', 'day': 'numeric'})
+
+
 
 
 	return (
@@ -24,9 +26,90 @@ const PodcastDetail = () => {
                 <h3>
                     {filter.data.podcasts.data[0].title}
                     <div className='colorBar'>
-
-                </div>
+                    </div>
                 </h3>
+                <table className='podInfoOuter'>
+                    <tbody>
+                        <tr>
+                            <td className='verticalAlignTop'>
+                                <table className='podInfo'>
+                                    <tbody>
+                                        <tr>
+                                            <th className='infoHeader'>Creator</th>
+                                            <td colSpan={2} className='creator'>
+                                                {filter.data.podcasts.data[0].author.name}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Type</th>
+                                            <td colSpan={2} className='regular'>
+                                                Podcast
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Released</th>
+                                            <td colSpan={2} className='regular'>
+                                                {formattedDateShort(filter.data.podcasts.data[0].startDate)}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Latest Ep</th>
+                                            <td colSpan={2} className='regular'>
+                                                {formattedDateLong(filter.data.podcasts.data[0].latestEpisodeDate)}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader p4'>Rating</th>
+                                            <td colSpan={2}>
+                                                <span className='avgRating regular'>
+                                                    {filter.data.podcasts.data[0].ratingAverage}&nbsp;
+                                                </span>
+                                                <span className='maxRating regular'>
+                                                    /&nbsp; 
+                                                    <span>
+                                                        5.0&nbsp;
+                                                    </span>
+                                                </span>
+                                                <span className='numRatings'>
+                                                    from&nbsp;
+                                                    <b>
+                                                        <span>{filter.data.podcasts.data[0].ratingCount}&nbsp;</span>
+                                                    </b>
+                                                    ratings
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Episodes #</th>
+                                            <td colSpan={2} className='regular'>
+                                                {filter.data.podcasts.data[0].numberOfEpisodes}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Avg Length</th>
+                                            <td colSpan={2} className='regular'>
+                                                {formattedTime(filter.data.podcasts.data[0].avgEpisodeLength)}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Description</th>
+                                            <td colSpan={2} className='desc'>
+                                                {filter.data.podcasts.data[0].description}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th className='infoHeader'>Launch</th>
+                                            <td colSpan={2}>
+                                                <a className='mediaSpotify' title='Spotify' target='_blank' href={filter.data.podcasts.data[0].webUrl}></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
             </div>
             <div className='mediaContainer'>
                 <a className='mediaTwitter' title='Twitter' target='_blank' href={`https://twitter.com/${filter.data.podcasts.data[0].socialLinks.twitter}`}> </a>
@@ -35,26 +118,7 @@ const PodcastDetail = () => {
             </div>
             <div className='colorBar'>
             </div>
-
-            </div>
-
-
-        
-		// <div className='videoCard'>
-        //     <div className='likesAdd'><PlaylistAdd className='videoIcon'/></div>
-		// 	<img className='videoImg' src={filter.data.podcasts.data[0].imageUrl}/>
-        //     <div className='duration'>
-        //     {formattedTime(filter.data.podcasts.data[0].avgEpisodeLength)}
-        //     </div>
-		// 	<div className='videoData'>
-        //     <div className='channelDetails'>
-        //     <h5>{filter.data.podcasts.data[0].title}</h5>
-        //     <p className='c_name'><img className='twitter' src='icons8-twitter.svg'/> {filter.data.podcasts.data[0].socialLinks.twitter}</p>
-        //     <p>{formattedTime(filter.data.podcasts.data[0].avgEpisodeLength)} Average</p>
-        //     </div>
-        //     <MoreVert className='moreIcon'/>
-		// 	</div>
-		// </div>
+        </div>
 	)
 }
 
